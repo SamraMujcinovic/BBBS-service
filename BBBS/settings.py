b999ffef91be592ee45cfa92fbc8fb6e02b6784c
SamraMujcinovic <smujcinovi1@etf.unsa.ba>
2024-08-03 15:53:10 +0200

    db deployment

--- BBBS/settings.py
+++ BBBS/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 from datetime import timedelta
 from pathlib import Path
+import os
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -93,16 +95,22 @@ WSGI_APPLICATION = "BBBS.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+#DATABASES = {
+ #   "default": {
+  #      "ENGINE": "django.db.backends.postgresql_psycopg2",
+   #     "NAME": "bbbs",
+    #    "USER": "bbbsuser",
+     #   "PASSWORD": "password",
+      #  "HOST": "localhost",
+       # "PORT": "5432",
+  #  }
+#}
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.postgresql_psycopg2",
-        "NAME": "bbbs",
-        "USER": "bbbsuser",
-        "PASSWORD": "password",
-        "HOST": "localhost",
-        "PORT": "5432",
-    }
+    'default': dj_database_url.config(
+        default=os.getenv('DATABASE_URL')
+    )
 }
+
 
 REST_FRAMEWORK = {
     "DEFAULT_PERMISSION_CLASSES": [
