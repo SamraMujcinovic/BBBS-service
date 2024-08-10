255981c4d1993af023375f47052ebd65cceddc97
SamraMujcinovic <smujcinovi1@etf.unsa.ba>
2024-08-10 11:42:26 +0200

    changes regarding smtp

--- BBBS/settings.py
+++ BBBS/settings.py
@@ -32,6 +32,7 @@ EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = "smtp.gmail.com"
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
+EMAIL_USE_SSL = False
 EMAIL_HOST_USER = "bbbs.organisation@gmail.com"
 EMAIL_HOST_PASSWORD = "fzjvawpzgtbebcbz"
 DEFAULT_FROM_EMAIL = "SBSS Organizacijski Tim <noreply@bbbs.com>"
