f09608def7967e38f01395b7b0f2051cef7d87b7
SamraMujcinovic <smujcinovi1@etf.unsa.ba>
2024-08-27 22:58:35 +0200

    access token test done

--- BBBS/settings.py
+++ BBBS/settings.py
@@ -117,8 +117,8 @@ REST_FRAMEWORK = {
 }
 
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=2),
-    'REFRESH_TOKEN_LIFETIME': timedelta(minutes=5),
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=5),
+    'REFRESH_TOKEN_LIFETIME': timedelta(hours=8),
     'ROTATE_REFRESH_TOKENS': False,
     'BLACKLIST_AFTER_ROTATION': True,
     'UPDATE_LAST_LOGIN': False,
